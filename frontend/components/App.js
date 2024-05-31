2a0650aea59fba0d68dc018b881a42c02917bb73
Lun Ng <lunngbill@gmail.com>
2024-05-31 16:20:35 -0700

    finished

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -8,11 +8,25 @@ const urlPeople = 'http://localhost:9009/api/people'
 function App() {
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+  const [data, setData] = useState([])
+  useEffect(() => {
+    async function getData() {
+      const [resPeople, resPlanets] = await Promise.all([axios.get(urlPeople), axios.get(urlPlanets)])
+      let characters = resPeople.data.map(char => {
+        return {...char, homeworld: resPlanets.data.find(planet => planet.id == char.homeworld)}
+      })
+      setData(characters)
+    }
+    getData()
+  }, [])
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      { 
+        data.map(char => <Character key={char.id} data={char}/>)
+      }
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
+function Character({data}) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const [showHomeworld, setShowHomeworld] = useState(false)
+  const Homeworldtoggle = () => setShowHomeworld(!showHomeworld)
   return (
-    <div>
+    <div className='character-card' onClick={Homeworldtoggle}>
+      <h3 className='character-name'>{data.name}</h3>
+      {showHomeworld && <p>Planet: <span className='character-planet'>{data.homeworld.name}</span></p>}
       {/* Use the same markup with the same attributes as in the mock */}
     </div>
   )
